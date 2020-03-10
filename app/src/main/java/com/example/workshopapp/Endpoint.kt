60f8d67a1423c203f7cca87c5793edfc4085daec
Humberto Linero <17732781+hlinero@users.noreply.github.com>
2020-03-10 14:28:30 +0100

    Fixing the endpoint file to return the correct type, correctly initializing the viewmodel, configuring the progress bar in the main activity, adding an id to the progressbar and recycler view

--- app/src/main/java/com/example/workshopapp/Endpoint.kt
+++ app/src/main/java/com/example/workshopapp/Endpoint.kt
@@ -1,15 +1,14 @@
 package com.example.workshopapp
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface Endpoint {
-    @Headers("Authorization: ${Credentials}")
+    @Headers("Authorization: ${Credentials.VASTTRAFIK_KEY}")
     @GET("bin/rest.exe/v2/location.nearbystops")
     fun getNearbyStops(@Query("originCoordLat") latitude: Double,
                        @Query("originCoordLong") longitude: Double,
-                       @Query("format") format: String) {
-
-    }
+                       @Query("format") format: String) : Call<StopData>
 }
